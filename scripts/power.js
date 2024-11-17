b9700a48f977147e34fbe2626ae73cdd3b211c0e
Haznobrain <143508779+Haznobrain@users.noreply.github.com>
2024-11-17 10:55:08 +0100

    Update power.js

--- scripts/power.js
+++ scripts/power.js
@@ -1,5 +1,5 @@
 Events.on(ClientLoadEvent, () => {
-  let block = Vars.content.getByName(ContentType.block, "modname-name");
+  let block = Vars.content.getByName(ContentType.block, "towerdefensemolten-06-p3-variation-generator");
   block.buildType = () => extend(PowerGenerator.GeneratorBuild, block, {
     getPowerProduction(){
       return Mathf.absin(120, 600); 
