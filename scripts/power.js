38212c63813c401b6dd52e4f6223b96f4f5e060c
Haznobrain <143508779+Haznobrain@users.noreply.github.com>
2024-11-17 10:57:26 +0100

    Update power.js

--- scripts/power.js
+++ scripts/power.js
@@ -2,7 +2,7 @@ Events.on(ClientLoadEvent, () => {
   let block = Vars.content.getByName(ContentType.block, "towerdefensemolten-06-p3-variation-generator");
   block.buildType = () => extend(PowerGenerator.GeneratorBuild, block, {
     getPowerProduction(){
-      return Mathf.absin(120, 600); 
+      return Mathf.absin(120, 10); 
     }
   });
 });
